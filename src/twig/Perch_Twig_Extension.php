30cd473a6581b05ca2bd3ed13bc9e8eef559b7cf
cvaldemar <casper@polarfrog.com>
2012-09-05 13:14:09 +0200

    Added perch_location calling PerchSystem::set_page, so Perch knows which Page is currently displayed.

--- src/twig/Perch_Twig_Extension.php
+++ src/twig/Perch_Twig_Extension.php
@@ -9,12 +9,18 @@ class Perch_Twig_Extension extends \Twig_Extension
 		return array(
 			'perch_runtime' => new Twig_Function_Method($this, 'runtime'),
 			'perch_content' => new Twig_Function_Method($this, 'content'),
+			'perch_location' => new Twig_Function_Method($this, 'location'),
 		);
 	}
 
 	public function content($region)
 	{
 		return perch_content($region);
+	}
+
+	public function location($set_page)
+	{
+		return PerchSystem::set_page($set_page);
 	}
 
 	public function runtime()
